4e4b3ae44604b2506bd95b0ce54efbb4559ca9d6
Dmitriy Tikhonin <dimansky777@gmail.com>
2021-12-06 16:26:16 +0300

    Free mile java project added

---
+++ src/freeMile.java
@@ -0,0 +1,8 @@
+public class freeMile {
+    public static void main(String[] args) {
+        int ticketPrice = 12001;
+        int freeMileBonus = ticketPrice / 20;
+        System.out.println("Сумма бонусных миль:" + freeMileBonus);
+    }
+}
+
